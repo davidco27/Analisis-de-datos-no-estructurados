b5f8336033450bf45131389e6341b9310d29d74b
davidco27 <74071922+davidco27@users.noreply.github.com>
2024-05-11 14:18:39 +0200

    P

--- Streamlit-image.py
+++ Streamlit-image.py
@@ -61,9 +61,9 @@ def load_resources():
 
 scratch_model,transfer_model = load_resources()
 
-st.sidebar.page_link(label="Inicio", page="https://www.marca.com"),
-st.sidebar.page_link(label="Imagen", page="https://no-estructurados-image.streamlit.app"),
-st.sidebar.page_link(label="Texto", page="https://no-estructurados-texto1.streamlit.app"),
+st.page_link(label="Inicio", page="https://www.marca.com"),
+st.page_link(label="Imagen", page="https://no-estructurados-image.streamlit.app"),
+st.page_link(label="Texto", page="https://no-estructurados-texto1.streamlit.app"),
 
 st.title("IMAGEN")
 st.subheader("Dataset de imágenes de 100 deportes")
